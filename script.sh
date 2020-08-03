b989d58302198a12c144ff008670d489d07b9ff5
SplunkVenkat <venkatfreeuse@gmail.com>
2020-08-03 15:33:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $status != "OK" ]; then
+if [ "$status" != "OK" ]; then
      echo -e "Quality Gate Failed. Please check the sonar for more info" && exit 1
   else       
      echo -e "Quality Gate Passed" && exit 0 
